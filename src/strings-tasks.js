22aebc179a9544de90d9e514b4b3888182dd5334
Diana Dukhovskaya <k.dianavv@gmail.com>
2025-09-14 17:03:54 +0100

    feat: complete task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -192,8 +192,14 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!str) return 0;
+  const strArray = str.split('');
+  let sum = 0;
+  for (let i = 0; i < strArray.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
@@ -239,8 +245,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const formatMins = minutes.toString().padStart(2, '0');
+  const formatSecs = seconds.toString().padStart(2, '0');
+  return `${formatMins}:${formatSecs}`;
 }
 
 /**
@@ -302,8 +310,14 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const strArray = str.toLowerCase().split('');
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  let vowelsNumber = 0;
+  for (let i = 0; i < strArray.length; i += 1) {
+    if (vowels.includes(strArray[i])) vowelsNumber += 1;
+  }
+  return vowelsNumber;
 }
 
 /**
